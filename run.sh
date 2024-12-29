2fdc8667ce1077bf212da5e8f0147d5d5d90a990
Junjie Yang <yangjunjie0320@gmail.com>
2024-12-29 17:13:34 +0800

    update

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 # export CXXFLAGS="-I/home/yangjunjie/opt/anaconda/include/"
 conda create -n simple-gemm python=3.10 anaconda
 conda activate simple-gemm
-conda install conda-forge::openblas
+conda install conda-forge::openblas conda-forge::benchmark
 
 export MKL_NUM_THREADS=1
 export OMP_NUM_THREADS=1
@@ -14,7 +14,7 @@ lscpu | grep "Cache size"
 export PREFIX=/home/yangjunjie/work/simple-gemm/;
 rm -rf $PREFIX/build/; mkdir $PREFIX/build/;
 cd $PREFIX/build/;
-cmake ..; make VERBOSE=1 -j4; cd -
+cmake -DN=64 ..; make VERBOSE=1 -j4; cd -
 
 cd $PREFIX/build/;
 for m in 256 512 1024 2048; do
